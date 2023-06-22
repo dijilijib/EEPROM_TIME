ccf716d82fa2604892f53c3f391b74aff1f8d85c
dijilijib <110416277+dijilijib@users.noreply.github.com>
2023-06-23 01:43:33 +0300

    Update RPC.cpp

--- RPC.cpp
+++ RPC.cpp
@@ -1,37 +1,20 @@
 #include "RPC.h"
 
-RPC::RPC()
-    : tb(espClient, MAX_MESSAGE_SIZE),
-    subscribed(false)
-{
-}
+#define ENCRYPTED false
+constexpr const char RPC_SWITCH_METHOD[] PROGMEM = "example_set_switch";
+constexpr const char RPC_SWITCH_KEY[] PROGMEM = "switch";
+constexpr const char RPC_RESPONSE_KEY[] PROGMEM = "example_response";
 
-void RPC::loop() {
-  if (!subscribed) {
-    Serial.println("Subscribing for RPC...");
-    if (!tb.RPC_Subscribe(callbacks.cbegin(), callbacks.cend())) {
-      Serial.println("Failed to subscribe for RPC");
-      return;
-    }
+class RPC {
+public:
+    RPC();
+    void loop();
 
-    Serial.println("Subscribe done");
-    subscribed = true;
-  }
-
-  tb.loop();
-}
-
-RPC_Response RPC::processSwitchChange(const RPC_Data &data) {
-  Serial.println("Received the set switch method");
-  const bool switch_state = data[RPC_SWITCH_KEY];
-  Serial.print("Example switch state: ");
-  Serial.println(switch_state);
-
-  StaticJsonDocument<JSON_OBJECT_SIZE(1)> doc;
-  doc[RPC_RESPONSE_KEY] = 22.02;
-  return RPC_Response(doc);
-}
-
-const std::array<RPC_Callback, 2U> RPC::callbacks = {
-  RPC_Callback{ RPC_SWITCH_METHOD, processSwitchChange }
+private:
+    WiFiClient espClient;
+    ThingsBoard tb;
+    bool subscribed;
+    RPC_Response processSwitchChange(const RPC_Data &data);
 };
+
+#endif //RPC_H
